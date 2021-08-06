b998711e68e96991bcc35412d7bdcda2e0393fe5
Arseniy <34647665+ArseniySilin@users.noreply.github.com>
2021-08-06 18:29:24 +0300

    Create clean.sh

---
+++ clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+fsLimit=80 # available fs usage percentage limit
+mountName="/foo/bar" # directory name for clearing
+
+percentage_used_int=$(grep -oE '[0-9]+%' <<<$(df ${mountName} | awk '{print $5}') | tr -d '%')
+
+if [[ $percentage_used_int -gt $fsLimit ]]
+then
+    # run cleaning commands here
+    echo "Disk space was cleared"
+else 
+    echo "Used disk space limit=${fsLimit}% is not reached, current usagee is ${percentage_used_int}%"
+fi 
