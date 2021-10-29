a8e3e56262731046a881b0681ac92fdca75cca2c
Arseniy <34647665+ArseniySilin@users.noreply.github.com>
2021-10-29 10:11:09 +0300

    Update daysWasNotModifiedClean.sh

--- daysWasNotModifiedClean.sh
+++ daysWasNotModifiedClean.sh
@@ -8,7 +8,7 @@ echo "[x] Cleaning started at $(date)"
 
 for BUILD_DIR in $(ls $BUILDS_DIR_ROOT);
 do
-        find "$BUILDS_DIR_ROOT/$BUILD_DIR" -mindepth 1 -maxdepth 1 -type d -mtime +"$DAYS_WAS_NOT_MODIFIED" -exec rm -rv {} \;
+        find "$BUILDS_DIR_ROOT/$BUILD_DIR" -mindepth 1 -maxdepth 1 -type d -mtime +"$DAYS_WAS_NOT_MODIFIED" -exec rm -rvf {} \;
 done
 
 echo "[x] Cleaning stopped"
